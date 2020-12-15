1b770c16a9634827a7b7d3b1903607bd4bf59417
Ivy Kay M. Gaddao <60841709+ivykayyy@users.noreply.github.com>
2020-12-15 12:33:59 +0800

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-function_alert("If you have questions, feel free to contact us");
-
-function function_alert($msg) {
-    echo "<script type='text/javascript'>alert('$msg');</script>";
-}
-?>
